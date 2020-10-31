2427ba6d4b33ccf21eb4f080e2e99f5196b7cf74
Vickie <Vickie.Blakelock@gmail.com>
2020-10-31 11:50:36 +0000

    allow user to add to db

--- app.py
+++ app.py
@@ -105,8 +105,20 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/add_recipe")
+@app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
+    if request.method == "POST":
+        recipe = {
+            "category_name": request.form.get("category_name"),
+            "recipe_name": request.form.get("recipe_name"),
+            "recipe_ingredients": request.form.get("recipe_ingredients"),
+            "recipe_method": request.form.get("recipe_method"),
+            "created_by": session["user"]
+        }
+        mongo.db.tasks.insert_one(recipe)
+        flash("Recipe Successfully Added")
+        return redirect(url_for("get_recipe"))
+
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_recipe.html", categories=categories)
 
